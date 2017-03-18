5d845667004b5b3cda1a0376a005282d33b3b7ea
sahir <sahir@punchh.com>
2017-03-18 23:20:28 +0530

    add i18n

--- i18n/index.js
+++ i18n/index.js
@@ -1,9 +1,8 @@
 import I18n from 'react-native-i18n';
 import English from './en-GB';
-import FrenchCanada from './fr-CA';
 
 // Translations
 I18n.fallbacks = true;
 I18n.translations = {
-  'en-GB': EnglishDefault,
+  'en-GB': English,
 }
